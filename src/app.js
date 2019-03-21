27d0be5f66091eba16bebe998c3a0c0f3ca6a36e
Miguel Encarnacion <miguelencarnacion7@gmail.com>
2019-03-21 20:29:59 +0000

    create basic Header component

--- src/app.js
+++ src/app.js
@@ -1,56 +1,11 @@
- 
-const app = {
-  title: 'Indecision App',
-  subtitle: "I'll help you choose what to do",
-  options: []
-};
-
-const onFormSubmit = (e) => {
-  e.preventDefault();
-  
-  const option = e.target.elements.option.value;
-  
-  if (option) {
-    app.options.push(option);
-    e.target.elements.option.value = '';
-    renderPage();
-  }
-};
-
-const removeAllOptions = () => {
-  app.options = [];
-  renderPage();
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <p>{this.props.subtitle}</p>
+      </div>
+    );
+ }
 }
 
-const chooseAnOption = () => {
-  let rndNumber = Math.floor(Math.random() * app.options.length);
-  let option = app.options[rndNumber];
-}
-
-const appRoot = document.getElementById('app');
-
-const renderPage = () => {
-  const template = (
-    <div>
-      <h1>{app.title}</h1>
-      {app.subtitle && <p>{app.subtitle}</p>}
-      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-      <button onClick={chooseAnOption} disabled={app.options.length === 0}>Choose an option</button>
-      <button onClick={removeAllOptions}>Remove All</button>
-      <ol>
-        {
-        app.options.map((option) => {
-          return <li key={option}>{option}</li>
-        })
-      }
-      </ol>
-      <form onSubmit={onFormSubmit}>
-        <input type="text" name="option" />
-        <button>Add option</button>
-      </form>
-    </div>
-  );
-  ReactDOM.render(template, appRoot);
-};
-
-renderPage();
