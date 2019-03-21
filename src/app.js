ad5a20ca2f8d3b5dcaac325c06d60b6070316a03
Miguel Encarnacion <miguelencarnacion7@gmail.com>
2019-03-21 20:31:27 +0000

    Create basic App wrapper component

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+  render() {
+    const title = "Indecision App";
+    const subtitle = "Put your life in the hands of a computer";
+    
+    return (
+      <div>
+        <Header title={title} subtitle={subtitle} />
+      </div>
+    );
+  }
+}
+
 class Header extends React.Component {
   render() {
     return (
